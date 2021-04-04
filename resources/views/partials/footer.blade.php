390bedb89217f54e252651d1127ce3a204dfc37e
Adnane-BENABDELLAH <adnaneiron9@gmail.com>
2021-04-04 18:01:03 +0100

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,17 @@
+<footer>
+    
+    <div class="footer">
+        <div class="container">
+            <div class="row footer-inner">
+                
+  
+                <div class="col-12 mt-5 text-center copyright">
+                    <p> 
+                        Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved
+                    </p>
+                </div>
+             </div>
+        </div>
+    </div >
+     <!-- end footer-content -->
+</footer>
